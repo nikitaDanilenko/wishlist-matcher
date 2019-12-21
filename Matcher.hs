170759d25b4914c435f6e666fac7c47669e1f86f
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-12-21 12:40:59 +0100

    Start working on actual queries

--- Matcher.hs
+++ Matcher.hs
@@ -1,5 +1,13 @@
 module Matcher where
 
+import Control.Monad.Trans.Class                  ( lift )
+import Control.Monad.Trans.Maybe                  ( MaybeT ( .. ), runMaybeT )
+import Data.Aeson                                 ( FromJSON ( parseJSON ), Value ( Object ),
+                                                    (.:), decode, Object )
+import Data.Aeson.Types                           ( Parser )
+import qualified Data.Text as Text                ( pack )
+
+
 data Friend = Profile Integer
             | Id String
             deriving (Eq, Show, Read)
@@ -7,3 +15,11 @@ data Friend = Profile Integer
 type Game = String
 
 data GameWish = GameWish { friend :: Friend, game :: Game }
+
+fetchWishList :: Friend -> MaybeT IO [Game]
+fetchWishList = undefined
+
+-- Shorthand for accessing fields in a JSON object.
+
+(.@) :: FromJSON a => Object -> String -> Parser a
+m .@ str = m .: Text.pack str
