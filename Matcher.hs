4af05f59a15b77540aad2a2f6334e5d843ef932a
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2019-12-21 12:23:41 +0100

    Fix type, add further types.

--- Matcher.hs
+++ Matcher.hs
@@ -1,6 +1,9 @@
 module Matcher where
 
-data Friend = Profile Long
+data Friend = Profile Integer
             | Id String
             deriving (Eq, Show, Read)
 
+type Game = String
+
+data GameWish = GameWish { friend :: Friend, game :: Game }
