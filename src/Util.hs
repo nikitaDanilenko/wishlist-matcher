b2275434b420f0d58e32328b14b7fc7df2ddb727
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2023-12-31 20:50:57 +0100

    Define friend info related functions

--- src/Util.hs
+++ src/Util.hs
@@ -1,9 +1,20 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Util where
 
-import Data.Aeson                                 ( FromJSON, Object, (.:) )
-import Data.Aeson.Key                             ( fromString )
-import Data.Aeson.Types                           ( Parser )
+import           Data.Aeson       (FromJSON, Object, (.:))
+import           Data.Aeson.Key   (fromString)
+import           Data.Aeson.TH    (defaultOptions, deriveJSON)
+import           Data.Aeson.Types (Parser)
 
 -- Shorthand for accessing fields in a JSON object.
 (.@) :: FromJSON a => Object -> String -> Parser a
 m .@ str = m .: fromString str
+
+newtype ApiKey = ApiKey { key :: String }
+
+-- The naming is consistent with the API
+newtype SteamID = SteamID { steamid :: String }
+    deriving Show
+
+deriveJSON defaultOptions ''SteamID
