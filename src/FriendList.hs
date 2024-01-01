140295c76e520f8231c2c30bdc67f5c2b5e684d9
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-01-01 23:45:53 +0100

    Use correct selection

--- src/FriendList.hs
+++ src/FriendList.hs
@@ -19,15 +19,16 @@ newtype FriendsResponse = FriendsResponse { friends :: [SteamID] }
 
 deriveJSON defaultOptions ''FriendsResponse
 
-fetchFriendList :: ApiKey -> SteamID -> IO FriendList
-fetchFriendList apiKey accountId = do
-    let friendListPath = concat ["http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=", key apiKey, "&steamid=", U.steamid accountId, "&relationship=friend"]
-    friendsJson <- simpleHttp friendListPath
-    let friendIds = fromMaybe [] (decode friendsJson :: Maybe [SteamID])
-    friendInfos <- mapM (fetchFriendInfo apiKey) friendIds
-    return (FriendList (catMaybes friendInfos))
-
 -- The response returned by the 'ISteamUser/GetFriendList' endpoint.
 newtype GetFriendsListResponse = GetFriendsListResponse { friendslist :: FriendsResponse }
 
 deriveJSON defaultOptions ''GetFriendsListResponse
+
+fetchFriendList :: ApiKey -> SteamID -> IO FriendList
+fetchFriendList apiKey accountId = do
+    let friendListPath = concat ["http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=", key apiKey, "&steamid=", U.steamid accountId, "&relationship=friend"]
+    friendsJson <- simpleHttp friendListPath
+    let response = decode friendsJson :: Maybe GetFriendsListResponse
+        friendIds = maybe [] (friends . friendslist) response
+    friendInfos <- mapM (fetchFriendInfo apiKey) friendIds
+    return (FriendList (catMaybes friendInfos))
