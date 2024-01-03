0409ffb9595cb52eab9051cb0e593f06630cb8f8
Nikita Danilenko <nikita.danilenko.is@gmail.com>
2024-01-03 20:49:48 +0100

    Remove unused function

--- src/Util.hs
+++ src/Util.hs
@@ -7,10 +7,6 @@ import           Data.Aeson.Key   (fromString)
 import           Data.Aeson.TH    (defaultOptions, deriveJSON)
 import           Data.Aeson.Types (Parser)
 
--- Shorthand for accessing fields in a JSON object.
-(.@) :: FromJSON a => Object -> String -> Parser a
-m .@ str = m .: fromString str
-
 newtype ApiKey = ApiKey { key :: String }
 
 -- The naming is consistent with the API
